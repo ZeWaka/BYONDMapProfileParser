adb10de41ed114ed667ad397686aaa2a5cdbf399
ZeWaka <zewakagamer@gmail.com>
2021-06-04 18:28:48 -0700

    clippy and cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::{env, fs};
 
-#[deny(clippy::pedantic)]
 #[derive(Serialize, Deserialize, Debug)]
 struct ProfileJSONEntry {
 	name: String,
@@ -46,7 +45,11 @@ fn main() {
 		let data: Option<Vec<ProfileJSONEntry>> = match serde_json::from_str(&contents) {
 			Ok(x) => x,
 			Err(e) => {
-				println!("Error encountered while parsing:{} \nFile: {}", fullpath.display(), e);
+				println!(
+					"Error encountered while parsing:{} \nFile: {}",
+					fullpath.display(),
+					e
+				);
 				None
 			}
 		};
@@ -84,7 +87,7 @@ fn main() {
 		let graph_path = format!("output/{}.png", sanitize_filename::sanitize(graph_name));
 
 		// Get maximum value out of our possible logs for max y graph bound
-		let mut max_data_value: OrderedFloat<f32> = OrderedFloat(0f32);
+		let mut max_data_value: OrderedFloat<f32> = OrderedFloat(0_f32);
 		for data in data.1.iter() {
 			max_data_value = max(max_data_value, data.value);
 		}
@@ -105,34 +108,51 @@ fn main() {
 
 		// Create chart elements
 		let mut chart = ChartBuilder::on(&root_area)
-			.caption(graph_name, (FontFamily::Serif, num::clamp(85 - graph_name.len(), 20, 50) as i32).into_font())
+			.caption(
+				graph_name,
+				(
+					FontFamily::Serif,
+					num::clamp(85 - graph_name.len(), 20, 50) as i32,
+				)
+					.into_font(),
+			)
 			.margin(20)
 			.margin_right(30)
 			.x_label_area_size(50)
 			.y_label_area_size(50)
-
 			// Give some room on the edges of the bounds
-			.build_cartesian_2d((min_time - 3600)..(max_time + 3600), 0f32..(f32::from(max_data_value) + (f32::from(max_data_value) / 20.0)))
+			.build_cartesian_2d(
+				(min_time - 3600)..(max_time + 3600),
+				0_f32..(f32::from(max_data_value) + (f32::from(max_data_value) / 20.0)),
+			)
 			.unwrap();
 
 		// Draw grid
-		chart.configure_mesh()
+		chart
+			.configure_mesh()
 			.y_desc("Seconds")
 			.x_desc("Roundend in UNIX time")
 			.draw()
 			.expect("Draw failure");
 
 		//Draw our actual data points
-		chart.draw_series(
-			data.1.iter().map(|data| Circle::new((data.time.timestamp(), *data.value), 3, RED.filled()))
-		).unwrap();
+		chart
+			.draw_series(
+				data.1
+					.iter()
+					.map(|data| Circle::new((data.time.timestamp(), *data.value), 3, RED.filled())),
+			)
+			.unwrap();
 
 		// Draw connecting line
-		chart.draw_series(
-			LineSeries::new(
-			data.1.iter().map(|data| (data.time.timestamp(), *data.value)),
-			&RED)
-		).unwrap();
+		chart
+			.draw_series(LineSeries::new(
+				data.1
+					.iter()
+					.map(|data| (data.time.timestamp(), *data.value)),
+				&RED,
+			))
+			.unwrap();
 
 		root_area.present().expect("Unable to write result to file, please make sure 'output' dir exists under current dir");
 	}
